e967f108193a69b9e08e6bad8321cfbae1159fef
Roman Malkov <r.malkov@vk.team>
2024-10-03 20:33:28 +0300

    fixed app-header adding header tag

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -9,34 +9,36 @@ import NavMenuItem from "../nav-menu-item";
 
 const AppHeader = () => {
   return (
-    <nav className={style.navbar}>
-      <div className={style.block_width}>
-        <ul className={style.ul}>
-          <li className={style.li_item}>
-            <NavMenuItem icon={BurgerIcon} name="Конструктор" isActive={true} />
-          </li>
-          <li className={style.li_item}>
+    <header className={style.app_header}>
+      <nav className={style.navbar}>
+        <div className={style.block_width}>
+          <ul className={style.ul}>
+            <li className={style.li_item}>
+              <NavMenuItem icon={BurgerIcon} name="Конструктор" isActive={true} />
+            </li>
+            <li className={style.li_item}>
+              <NavMenuItem
+                icon={ListIcon}
+                name="Лента заказов"
+                isActive={false}
+              />
+            </li>
+          </ul>
+        </div>
+        <div className={style.block_width}>
+          <Logo />
+        </div>
+        <div className={style.block_width}>
+          <span className={style.personal}>
             <NavMenuItem
-              icon={ListIcon}
-              name="Лента заказов"
+              icon={ProfileIcon}
+              name="Личный кабинет"
               isActive={false}
             />
-          </li>
-        </ul>
-      </div>
-      <div className={style.block_width}>
-        <Logo />
-      </div>
-      <div className={style.block_width}>
-        <span className={style.personal}>
-          <NavMenuItem
-            icon={ProfileIcon}
-            name="Личный кабинет"
-            isActive={false}
-          />
-        </span>
-      </div>
-    </nav>
+          </span>
+        </div>
+      </nav>
+    </header>
   );
 };
 
